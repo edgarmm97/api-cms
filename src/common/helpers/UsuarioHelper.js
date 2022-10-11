061a8d60b015ef24419a7c8f6b5264c6f5e60c2c
LuisCruzRamon <luisantonio.delacruzramon@gmail.com>
2022-10-10 22:15:40 -0500

    add: Helper para usuario con funcion hash para contrasenas

---
+++ src/common/helpers/UsuarioHelper.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcryptjs');
+
+const hashPassword = async password => {
+    const salt = await bcrypt.genSalt(10);
+    const password_hash = await bcrypt.hash(password, salt);
+    return password_hash;
+};
+module.exports = {
+    hashPassword,
+};
